792ba57e7a9cba30103069f66617a6db0c41f435
Ryan Nock <trury2000@gmail.com>
2019-11-13 12:56:04 -0600

    Initial Commit

---
+++ Boi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Boi
+//
+//  Created by P.M. Student on 11/13/19.
+//  Copyright © 2019 P.M. Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
